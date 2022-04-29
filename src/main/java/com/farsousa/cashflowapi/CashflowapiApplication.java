38e1f2c4cf778a92c8c7ac13a491c545d010c956
AndersonCaj26 <andersoncaj26@gmail.com>
2022-04-29 16:29:09 -0300

    estrutura básica do projeto

---
+++ src/main/java/com/farsousa/cashflowapi/CashflowapiApplication.java
@@ -0,0 +1,13 @@
+package com.farsousa.cashflowapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashflowapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashflowapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/farsousa/cashflowapi/CashflowapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farsousa.cashflowapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashflowapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
